dd2dc87ac72ec1d9c89864385ae7a548d723d4ff
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-05-18 19:58:38 +0300

    add 2-task and 3-task

---
+++ src/basik/2.ts
@@ -0,0 +1,6 @@
+type Person = [
+    name: string,
+    age: number
+]
+
+let person: Person = ['Max', 21];
---
+++ src/basik/3.ts
@@ -0,0 +1,6 @@
+// Створіть змінну, яка може містити або рядок, або число (union type)? 
+// Також, оголосіть змінну, яка може містити лише одне з двох можливих 
+// рядкових значень: 'enable' або 'disable' (literal type).
+
+const nameOrNumber : string | number;
+const enableOrDisable : 'enable' | 'disable';
