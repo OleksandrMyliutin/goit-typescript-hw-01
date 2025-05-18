03f772538b7d88a5166ae012f242f98e84c3676a
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-05-18 19:07:30 +0300

    add 1-task

---
+++ src/basik/1.ts
@@ -0,0 +1,10 @@
+const age: number = 50;
+const username: string = 'Max';
+const toggle: boolean = true;
+const empty: null = null;
+type A = {
+    param: number;
+}
+const callback = (a: A) => { 
+    return 100 + a.param;
+};
